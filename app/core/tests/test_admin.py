71d160780b8dc0fc074d25196ee4da5459562b2e
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2021-07-24 15:23:21 +0530

    Added support for custom user on admin page

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client, client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        ''' Sets up user and client '''
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@pankaj.com',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            name='test',
+            email='test@pankaj.com',
+            password='test123'
+        )
+
+    def test_user_listed(self):
+        ''' Tests that users are listed on users page '''
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_edit_page(self):
+        ''' Tests that user edit page works '''
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_create_page(self):
+        ''' Tests that user edit page works '''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
